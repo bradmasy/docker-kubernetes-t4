a3dbfc023d6dbaea5b528f31ab122762c743d7b2
unknown <bradmasy@gmail.com>
2023-02-22 20:01:57 -0800

    updated

---
+++ Multi-Container/working/complex/client/src/OtherPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// export default() => {
+//   return (
+//     <div>
+//       Im some other page!
+//       <Link to="/">Go back home</Link>
+//     </div>
+//   );
+// };
+
+const other = () => {
+  return(
+      <div>
+          The OTHER Page
+          <Link to="/">Home</Link>
+      </div>
+  );
+}
+
+export default other;
